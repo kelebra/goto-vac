2d03985acab4d9e09a660029a43983068d649b0b
kelebra <kelebra20@gmail.com>
2018-02-05 21:06:38 -0600

    Fixed incorrect initial connection

--- frontend/src/main/scala/com/gotovac/frontend/socket/Socket.scala
+++ frontend/src/main/scala/com/gotovac/frontend/socket/Socket.scala
@@ -2,22 +2,18 @@ package com.gotovac.frontend.socket
 
 import com.gotovac.frontend.pages.components.Notification
 import com.gotovac.model.Forbidden
+import org.scalajs.dom.MessageEvent
 import org.scalajs.dom.document.location
 import org.scalajs.dom.raw.{Event, WebSocket}
-import org.scalajs.dom.{MessageEvent, console}
 import prickle.Unpickle
 
 trait Socket {
 
-  val name: String
   private var underlying = refreshSocket()
 
   def connect(): Unit = {
     underlying = refreshSocket()
 
-    underlying.onopen = {
-      (_: Event) => console.log(s"$name - connection was opened")
-    }
     underlying.onmessage = {
       (e: MessageEvent) => {
         val json = e.data.toString
@@ -25,11 +21,7 @@ trait Socket {
         onMessage(json)
       }
     }
-    underlying.onclose = {
-      (_: Event) =>
-        console.log(s"$name - connection lost with backend")
-        connect()
-    }
+    underlying.onclose = (_: Event) => connect()
   }
 
   private def refreshSocket(): WebSocket = new WebSocket(socketUrl(name))
@@ -46,4 +38,6 @@ trait Socket {
   def send(json: String): Unit = underlying.send(json)
 
   def onMessage(json: String): Unit
+
+  def name: String
 }
